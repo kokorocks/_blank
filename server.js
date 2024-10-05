a6e20f735beaeb3453203b92f9c60b8db8367155
Ethan <175902687+kokorocks@users.noreply.github.com>
2024-10-05 14:30:51 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+import Peer from 'https://cdn.jsdelivr.net/npm/peerjs@1.4.0/dist/peerjs.min.js';
+
+// Create a new Peer instance
+const peers = new Peer();
+
+peer.on('open', (id) => {
+    console.log(`My peer ID is: ${id}`);
+});
